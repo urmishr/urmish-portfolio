1d6d5b47d604dd3241ba871dc0fc6d635f5b52c1
urmishr <ramani.urmish@gmail.com>
2025-04-22 00:22:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
+function App() {
+    return (
+        <div className="bg-stone-950 text-3xl h-[100vh] w-[100vw] flex flex-col justify-center items-center space-y-3">
+            <h1 className="text-white text-3xl">Hello baby 👋🏻</h1>
+            <Button
+                variant="outline"
+                className="w-[100px]"
+                onClick={() => toast("Himi My love I love you So much❤️")}
+            >
+                Click Me!
+            </Button>
+
+            <Toaster />
+        </div>
+    );
+}
+
+export default App;
